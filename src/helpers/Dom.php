4f40803d44d71c080d5bda3b0a59566c6d106f66
Marcus Aschan <marcusaschan@gmail.com>
2020-01-22 10:22:43 +0200

    set dom encoding to UTF-8

--- src/helpers/Dom.php
+++ src/helpers/Dom.php
@@ -19,7 +19,7 @@ class Dom
         $oldValue = libxml_disable_entity_loader(true);
 
         $dom = new DOMDocument();
-        $dom->loadHTML(trim($html));
+        $dom->loadHTML(mb_convert_encoding(trim($html), 'HTML-ENTITIES', 'UTF-8'));
 
         libxml_disable_entity_loader($oldValue);
         libxml_use_internal_errors($libxmlErrflag);
