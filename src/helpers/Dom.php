f0b76600fc8a9ec6aa3f5b0343f62741f86a596e
Martin Stigzelius <martin@agencyleroy.com>
2020-01-29 14:21:06 +0200

    Remove Domdocument from wrapping html with body and html tags

--- src/helpers/Dom.php
+++ src/helpers/Dom.php
@@ -19,7 +19,7 @@ class Dom
         $oldValue = libxml_disable_entity_loader(true);
 
         $dom = new DOMDocument('1.0', 'UTF-8');
-        $dom->loadHTML(mb_convert_encoding(trim($html), 'HTML-ENTITIES', 'UTF-8'));
+        $dom->loadHTML(mb_convert_encoding(trim($html), 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
 
         libxml_disable_entity_loader($oldValue);
         libxml_use_internal_errors($libxmlErrflag);
